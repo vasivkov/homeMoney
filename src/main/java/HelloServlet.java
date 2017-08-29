aa020ac5e1dd024ede7b2ee398446815e13a1a55
vasivkov <sivkov_va@mail.ru>
2017-08-30 02:49:54 +0300

    refactoring web.xml and pom.xml

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -7,6 +7,6 @@ import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
     public void doGet (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
-        httpServletResponse.getWriter().print("Hello");
+        httpServletResponse.getWriter().print("Hello, world!!!");
     }
 }
