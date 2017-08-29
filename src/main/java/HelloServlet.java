affeab12499f8de7e4847ae244f845fbbc91d607
vasivkov <sivkov_va@mail.ru>
2017-08-29 15:49:33 +0300

    initial commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,12 @@
+import javax.servlet.http.*;
+import java.io.IOException;
+
+/**
+ * Created by vasya on 29/08/17.
+ */
+
+public class HelloServlet extends HttpServlet {
+    public void doGet (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.getWriter().print("Hello");
+    }
+}
