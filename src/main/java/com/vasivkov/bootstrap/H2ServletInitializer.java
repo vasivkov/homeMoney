88f823068e0c959bd07a5190e45ed85582b97036
vasivkov <sivkov_va@mail.ru>
2017-09-03 19:51:32 +0300

    connecting H2 database to project

--- src/main/java/com/vasivkov/bootstrap/H2ServletInitializer.java
+++ src/main/java/com/vasivkov/bootstrap/H2ServletInitializer.java
@@ -4,8 +4,10 @@ import org.h2.tools.Server;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import java.sql.*;
 
+@WebListener
 public class H2ServletInitializer implements ServletContextListener {
     private static final String[] H2_TCP_SERVER_ARGS = {"-tcp", "-tcpAllowOthers", "-tcpPort", "9092"};
     private static final String[] H2_WEB_SERVER_ARGS = {"-web", "-webAllowOthers", "-webPort", "8082"};
@@ -17,21 +19,10 @@ public class H2ServletInitializer implements ServletContextListener {
         try {
             tcpServer = Server.createTcpServer(H2_TCP_SERVER_ARGS).start();
             webServer = Server.createWebServer(H2_WEB_SERVER_ARGS).start();
+            H2JdbcConnectionPool.getInstance().init();
         } catch (SQLException e1) {
             e1.printStackTrace();
             throw new RuntimeException(e1);
-        }
-        try {
-            Connection conn = DriverManager.getConnection("jdbc:h2:mem:test", "sa", "");
-            Statement st = conn.createStatement();
-            st.execute("create table customer(id INT primary key, name  varchar(10))");
-            ResultSet rset = st.executeQuery("SHOW TABLES");
-            while (rset.next()) {
-                String name = rset.getString(1);
-                System.out.println("TABLE NAME IS " + name);
-            }
-        } catch (SQLException e1) {
-            e1.printStackTrace();
         }
     }
 
@@ -44,6 +35,4 @@ public class H2ServletInitializer implements ServletContextListener {
             webServer.stop();
         }
     }
-
 }
-
